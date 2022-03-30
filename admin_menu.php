adc5cf936b017bb5dae37613d95a61cfce948b57
Cristy97 <89489800+Cristy97@users.noreply.github.com>
2022-03-29 18:03:42 -0600

    Modificacion del diseño

---
+++ admin_menu.php
@@ -0,0 +1,15 @@
+<div class="col-lg-4 col-md-12" style="background-color: #144653">
+            <a href="../paginas_admin/catalogo_encuesta.html
+            " class="browse-topic-tile opciones one"  tabindex="0" style="background-color: #D0D0D0" >
+            Encuestas
+        </a>
+        <a href="paginas_admin/catalogo_preg.html" class="browse-topic-tile opciones"  tabindex="0" style="background-color:#D0D0D0">
+            Preguntas
+        </a>
+        <a href="paginas_admin/respuestas.html" class="browse-topic-tile opciones"  tabindex="0" style="background-color: #D0D0D0">
+            Respuestas
+        </a>
+        <a href="paginas_admin/inicio.html" class="browse-topic-tile opciones"  tabindex="0" style="background-color: #D0D0D0">
+            Inicio
+        </a>
+</div>
