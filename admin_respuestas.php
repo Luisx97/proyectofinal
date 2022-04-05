5d8d6fc9b9be928dfbdd2f232242252e4d9b90d8
MarielaMtz <95498312+MarielaMtz@users.noreply.github.com>
2022-04-05 14:36:20 -0500

    Anexo del catalogo de preguntas/respuestas
    Anexo del formulario del catalogo de respuestas y preguntas

--- admin_respuestas.php
+++ admin_respuestas.php
@@ -25,6 +25,39 @@
             ?>
                 <div class="col-lg-8 col-md-12">
                     <h1 class="blog_bvn">Respuestas</h1>
+                    <br><br>
+                        <h4 class="blog_bvn">Periodo</h4>
+                        <input class="form-control" type="text" name="anio_nombre" id="anio_nombre"
+                            placeholder="Seleccione el periodo"
+                            aria-label="default input example">
+                        <br>
+                        <h4 class="blog_bvn">Encuesta</h4>
+                        <input class="form-control" type="text" name="encuesta_titulo" id="encuesta_titulo"
+                            placeholder="Seleccione encuesta"
+                            aria-label="default input example">
+                        <br>
+                        <div id="tabla_preguntas">
+                     <table class="table table-bordered table-hover table-condensed">
+                        <thead class="thead-dark">
+                           <tr>
+                             <th>Respuesta</th>
+                             <th>Opcion</th>
+                             <th>Accciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           <tr>
+                              <td>Aquì traer pregunta</td>
+                              <td><a href="#feature">Opcion</a></td>
+                              <td>
+                                 <button //onclick="Agregaropcion(2)"// class="btn btn-success">Agregar</button>   
+                                 <button //onclick="obtenerDetallesOpcion(2)"// class="btn btn-warning">Modificar</button>
+                                  <button //onclick="eliminarOpcion(2)"// class="btn btn-danger">Eliminar</button>
+
+                                </td>
+                            </tr>
+                         </tbody>
+                     </table>
                 </div>
             </div>
         </div>
