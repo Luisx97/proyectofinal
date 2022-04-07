480d148063d7580fe4f7b659e4d4ad51d7f2833c
Luis Fernando Escalante Ayala <luis.escalante@corporativo.local>
2022-04-07 17:06:50 -0500

    Ćreación del modulo alta_preguntas, el proyecto ya puede guardar las preguntas tomando en cuenta las encuestas que ya fuerón creadas

--- admin_catalogo_encuesta.php
+++ admin_catalogo_encuesta.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
-  date_default_timezone_set("America/Lima");
-  $date = new DateTime();
+date_default_timezone_set("America/Lima");
+$date = new DateTime();
 
-  $fecha_inicio = $date->format('Y-m-d H:i:s');
-  
- ?>
+$fecha_inicio = $date->format('Y-m-d H:i:s');
+
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -17,7 +17,7 @@
     <meta name="author" content="" />
     <title>Poder Judicial</title>
     <?php
-        include ("include/head.php");
+    include("include/head.php");
     ?>
 </head>
 
@@ -31,12 +31,12 @@
     <main class="flex-shrink-0">
         <!-- Navigation-->
         <?php
-            include ("include/navbar.php");
+        include("include/navbar.php");
         ?>
         <div class="container-fluid ">
             <div class="row">
                 <?php
-                    include ("include/admin_menu.php");
+                include("include/admin_menu.php");
                 ?>
                 <div class="col-lg-8 col-md-12">
                     <form name="form-work" action="scripts/encuesta_guardar.php" method="POST">
@@ -44,28 +44,24 @@
                         <h1 class="blog_bvn">Catálogo de Encuestas</h1>
                         <br></br>
                         <h4 class="blog_bvn">Titulo de Encuesta</h4>
-                        <input class="form-control" name="encuesta_titulo" id="encuesta_titulo" type="text"
-                            placeholder="Nombre de la encuesta" aria-label="default input example">
+                        <input class="form-control" name="encuesta_titulo" id="encuesta_titulo" type="text" placeholder="Nombre de la encuesta" aria-label="default input example">
                         <br>
                         <h4 class="blog_bvn">Periodo</h4>
                         <select class="form-control form-control-sm" name="anio_nombre" id="anio_nombre">
                             <option value="">Seleccione el periodo</option>
                             <?php
-                                while ($dataSelect = mysqli_fetch_array($datacat_status_aniosSelect)) { ?>
-                            <option value="<?php echo $dataSelect["anio_nombre"]; ?>">
-                                <?php echo ($dataSelect["anio_nombre"]); ?>
-                            </option>
+                            while ($dataSelect = mysqli_fetch_array($datacat_status_aniosSelect)) { ?>
+                                <option value="<?php echo $dataSelect["anio_nombre"]; ?>">
+                                    <?php echo ($dataSelect["anio_nombre"]); ?>
+                                </option>
                             <?php } ?>
                         </select>
                         <br>
                         <h4 class="blog_bvn">Descripción de la Encuesta</h4>
-                        <input class="form-control" type="text" name="encuesta_descripcion" id="encuesta_descripcion"
-                            placeholder="Ingresa una breve descripción de la encuesta"
-                            aria-label="default input example">
+                        <input class="form-control" type="text" name="encuesta_descripcion" id="encuesta_descripcion" placeholder="Ingresa una breve descripción de la encuesta" aria-label="default input example">
                         <br>
                         <h4 class="blog_bvn">Fecha de Cierre</h4>
-                        <input class="form-control" name="encuesta_fecha_final" id="encuesta_fecha_final" type="date"
-                            placeholder="" aria-label="default input example">
+                        <input class="form-control" name="encuesta_fecha_final" id="encuesta_fecha_final" type="date" placeholder="" aria-label="default input example">
                         <br>
                         <div class="d-grid gap-2 d-md-block">
                             <input class="btn btn-primary" type="submit" value="Enviar">
@@ -79,8 +75,8 @@
 </body>
 <footer class="bg-dark py-4 mt-auto">
     <?php
-            include ("include/footer.php")
-        ?>
+    include("include/footer.php")
+    ?>
 </footer>
 
 </div>
--- admin_catalogo_preg.php
+++ admin_catalogo_preg.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -10,75 +9,90 @@
 
     <title>Encuesta</title>
     <?php
-        include ("include/head.php");
+    include("include/head.php");
     ?>
     <link rel="stylesheet" type="text/css" href="libs/bootstrap/css/estilos.css">
 </head>
 
 <body>
+    <?php
+    include("con_db.php");
+    ?>
+
+
+
     <main class="flex-shrink-0">
         <!-- Navigation-->
         <?php
-            include ("include/navbar.php");
+        include("include/navbar.php");
         ?>
         <div class="container-fluid ">
             <div class="row">
-            <?php
-                include ("include/admin_menu.php");
-            ?> 
-            <br><br>     
+                <?php
+                include("include/admin_menu.php");
+                ?>
                 <div class="col-lg-8 col-md-12">
-                    <h1 class="blog_bvn">Catálogo de Preguntas</h1>
-                    <br><br>
-                    <h4 class="blog_bvn">Titulo pregunta</h4>
-                    <input class="form-control" name="pregunta_titulo" id="pregunta_titulo" type="text"
-                            placeholder="Nombre de la pregunta" aria-label="default input example">
-                    <br>
-                    <h4 class="blog_bvn">Variable de la pregunta</h4>
-                    <input class="form-control" name="pregunta_variable" id="pregunta_variable" type="text"
-                            placeholder="Variable de la pregunta" aria-label="default input example">
-                    <br>
-                    <h4 class="blog_bvn">Fecha de Cierre</h4>
-                        <input class="form-control" name="pregunta_fecha_final" id="pregunta_fecha_final" type="date"
-                            placeholder="" aria-label="default input example">
+                    <form name="form-work" action="scripts/pregunta_guardar.php" method="POST">
+
+                        <h1 class="blog_bvn">Catálogo de Preguntas</h1>
+                        <br></br>
+                        <h4 class="blog_bvn">Encuesta</h4>
+                        <select class="form-control form-control-sm" name="pregunta_titulo" id="pregunta_titulo">
+                            <option value="">Seleccione el periodo</option>
+                            <?php
+                            while ($dataSelect = mysqli_fetch_array($datacat_encuestaSelect)) { ?>
+                                <option value="<?php echo $dataSelect["encuesta_titulo"]; ?>">
+                                    <?php echo ($dataSelect["encuesta_titulo"]); ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                        <br>
+                        <h4 class="blog_bvn">Pregunta/Afirmación</h4>
+                        <input class="form-control" type="text" name="pregunta_texto" id="pregunta_texto" placeholder="Ingresa la pregunta que se quiere registrar" aria-label="default input example">
+                        <br>
+                        <h4 class="blog_bvn">Variable de Pregunta</h4>
+                        <input class="form-control" type="text" name="pregunta_variable" id="pregunta_variable" placeholder="Ingresa una breve descripción de la encuesta" aria-label="default input example">
+                        <br>
+                        <h4 class="blog_bvn">Fecha de Cierre</h4>
+                        <input class="form-control" name="pregunta_fecha_final" id="pregunta_fecha_final" type="date" placeholder="" aria-label="default input example">
                         <br>
                         <div class="d-grid gap-2 d-md-block">
-                            <input class="btn btn-primary" type="submit" value="Agregar pregunta">
+                            <input class="btn btn-primary" type="submit" value="Enviar">
                         </div>
                         <br>
-                      <div id="tabla_preguntas">
-                     <table class="table table-bordered table-hover table-condensed">
-                        <thead class="thead-dark">
-                           <tr>
-                             <th>Título</th>
-                             <th>Tipo</th>
-                             <th>Accciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                           <tr>
-                              <td><a href="#feature">Aquì traer tìtulo</a></td>
-                              <td>Selección múltiple</td>
-                              <td>
-                                 <button //onclick="obtenerDetallesPregunta(2)"// class="btn btn-warning">Modificar</button>
-                                  <button //onclick="eliminarPregunta(2)"// class="btn btn-danger">Eliminar</button>
-                                </td>
-                            </tr>
-                         </tbody>
-                     </table>
+                    </form>
+                    <div id="tabla_preguntas">
+                        <table class="table table-bordered table-hover table-condensed">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Título</th>
+                                    <th>Tipo</th>
+                                    <th>Accciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="#feature">Aquì traer tìtulo</a></td>
+                                    <td>Selección múltiple</td>
+                                    <td>
+                                        <button //onclick="obtenerDetallesPregunta(2)" // class="btn btn-warning">Modificar</button>
+                                        <button //onclick="eliminarPregunta(2)" // class="btn btn-danger">Eliminar</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-    </div>
-        
-</body>
- 
-
-
-        <?php
-            include ("include/footer.php")
-        ?>
-
         </div>
+</body>
+
+
+
+<?php
+include("include/footer.php")
+?>
+
+</div>
 
 </html>
